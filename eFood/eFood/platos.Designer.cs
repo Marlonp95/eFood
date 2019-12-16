a5af2579a6241ce6b314989efe16811c89e86046
Marlonp95 <marlonp95@hotmail.com>
2019-12-16 03:07:06 -0400

    son las 3 am

--- eFood/eFood/platos.Designer.cs
+++ eFood/eFood/platos.Designer.cs
@@ -33,12 +33,9 @@
             this.txtplato = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.porcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.unidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.unidad_medida = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.comboCategoria = new System.Windows.Forms.ComboBox();
+            this.categoriaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.efoodDataSet10 = new eFood.efoodDataSet10();
             this.button8 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button13 = new System.Windows.Forms.Button();
@@ -54,12 +51,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.comboReceta = new System.Windows.Forms.ComboBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.efoodDataSet10 = new eFood.efoodDataSet10();
-            this.categoriaBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.categoriaTableAdapter = new eFood.efoodDataSet10TableAdapters.categoriaTableAdapter();
-            this.efoodDataSet11 = new eFood.efoodDataSet11();
             this.recetasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.efoodDataSet11 = new eFood.efoodDataSet11();
+            this.label5 = new System.Windows.Forms.Label();
+            this.categoriaTableAdapter = new eFood.efoodDataSet10TableAdapters.categoriaTableAdapter();
             this.recetasTableAdapter = new eFood.efoodDataSet11TableAdapters.recetasTableAdapter();
             this.label6 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
@@ -69,12 +64,21 @@
             this.label10 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
+            this.codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.plato = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itbis = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.receta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button7 = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.efoodDataSet10)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.categoriaBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.efoodDataSet11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.efoodDataSet10)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.recetasBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.efoodDataSet11)).BeginInit();
             this.SuspendLayout();
             // 
             // txtplato
@@ -108,42 +112,18 @@
             this.dataGridView1.ColumnHeadersVisible = false;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.codigo,
+            this.plato,
             this.descripcion,
-            this.porcion,
-            this.unidad,
-            this.unidad_medida});
+            this.itbis,
+            this.receta,
+            this.categoria,
+            this.Column1});
             this.dataGridView1.Location = new System.Drawing.Point(11, 305);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(1039, 155);
             this.dataGridView1.TabIndex = 29;
-            // 
-            // codigo
-            // 
-            this.codigo.HeaderText = "codigo";
-            this.codigo.Name = "codigo";
-            // 
-            // descripcion
-            // 
-            this.descripcion.HeaderText = "descripcion";
-            this.descripcion.Name = "descripcion";
-            // 
-            // porcion
-            // 
-            this.porcion.HeaderText = "porcion";
-            this.porcion.Name = "porcion";
-            // 
-            // unidad
-            // 
-            this.unidad.HeaderText = "unidad";
-            this.unidad.Name = "unidad";
-            this.unidad.Visible = false;
-            // 
-            // unidad_medida
-            // 
-            this.unidad_medida.HeaderText = "unidad_medida";
-            this.unidad_medida.Name = "unidad_medida";
             // 
             // comboCategoria
             // 
@@ -156,6 +136,16 @@
             this.comboCategoria.Size = new System.Drawing.Size(194, 21);
             this.comboCategoria.TabIndex = 30;
             this.comboCategoria.ValueMember = "ID_CATEGORIA";
+            // 
+            // categoriaBindingSource
+            // 
+            this.categoriaBindingSource.DataMember = "categoria";
+            this.categoriaBindingSource.DataSource = this.efoodDataSet10;
+            // 
+            // efoodDataSet10
+            // 
+            this.efoodDataSet10.DataSetName = "efoodDataSet10";
+            this.efoodDataSet10.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // button8
             // 
@@ -316,6 +306,16 @@
             this.comboReceta.TabIndex = 184;
             this.comboReceta.ValueMember = "id_receta";
             // 
+            // recetasBindingSource
+            // 
+            this.recetasBindingSource.DataMember = "recetas";
+            this.recetasBindingSource.DataSource = this.efoodDataSet11;
+            // 
+            // efoodDataSet11
+            // 
+            this.efoodDataSet11.DataSetName = "efoodDataSet11";
+            this.efoodDataSet11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
             // label5
             // 
             this.label5.AutoSize = true;
@@ -326,29 +326,9 @@
             this.label5.TabIndex = 185;
             this.label5.Text = "Receta";
             // 
-            // efoodDataSet10
-            // 
-            this.efoodDataSet10.DataSetName = "efoodDataSet10";
-            this.efoodDataSet10.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // categoriaBindingSource
-            // 
-            this.categoriaBindingSource.DataMember = "categoria";
-            this.categoriaBindingSource.DataSource = this.efoodDataSet10;
-            // 
             // categoriaTableAdapter
             // 
             this.categoriaTableAdapter.ClearBeforeFill = true;
-            // 
-            // efoodDataSet11
-            // 
-            this.efoodDataSet11.DataSetName = "efoodDataSet11";
-            this.efoodDataSet11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // recetasBindingSource
-            // 
-            this.recetasBindingSource.DataMember = "recetas";
-            this.recetasBindingSource.DataSource = this.efoodDataSet11;
             // 
             // recetasTableAdapter
             // 
@@ -371,7 +351,7 @@
             this.label11.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.label11.Location = new System.Drawing.Point(11, 277);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(178, 25);
+            this.label11.Size = new System.Drawing.Size(156, 25);
             this.label11.TabIndex = 187;
             this.label11.Text = "Codigo";
             this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -381,7 +361,7 @@
             this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(91)))), ((int)(((byte)(140)))));
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label7.Location = new System.Drawing.Point(186, 277);
+            this.label7.Location = new System.Drawing.Point(149, 277);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(180, 25);
             this.label7.TabIndex = 188;
@@ -393,7 +373,7 @@
             this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(91)))), ((int)(((byte)(140)))));
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label8.Location = new System.Drawing.Point(360, 277);
+            this.label8.Location = new System.Drawing.Point(304, 277);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(180, 25);
             this.label8.TabIndex = 189;
@@ -405,7 +385,7 @@
             this.label9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(91)))), ((int)(((byte)(140)))));
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label9.Location = new System.Drawing.Point(519, 277);
+            this.label9.Location = new System.Drawing.Point(467, 277);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(140, 25);
             this.label9.TabIndex = 190;
@@ -417,7 +397,7 @@
             this.label10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(91)))), ((int)(((byte)(140)))));
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label10.Location = new System.Drawing.Point(650, 277);
+            this.label10.Location = new System.Drawing.Point(593, 277);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(136, 25);
             this.label10.TabIndex = 191;
@@ -429,9 +409,9 @@
             this.label12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(91)))), ((int)(((byte)(140)))));
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label12.Location = new System.Drawing.Point(915, 277);
+            this.label12.Location = new System.Drawing.Point(883, 277);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(136, 25);
+            this.label12.Size = new System.Drawing.Size(167, 25);
             this.label12.TabIndex = 192;
             this.label12.Text = "Categoria";
             this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -441,18 +421,78 @@
             this.label13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(91)))), ((int)(((byte)(140)))));
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label13.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label13.Location = new System.Drawing.Point(759, 277);
+            this.label13.Location = new System.Drawing.Point(717, 277);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(177, 25);
             this.label13.TabIndex = 193;
             this.label13.Text = "Receta";
             this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // codigo
+            // 
+            this.codigo.HeaderText = "id_plato";
+            this.codigo.Name = "codigo";
+            // 
+            // plato
+            // 
+            this.plato.HeaderText = "plato";
+            this.plato.Name = "plato";
+            // 
+            // descripcion
+            // 
+            this.descripcion.HeaderText = "descripcion";
+            this.descripcion.Name = "descripcion";
+            // 
+            // itbis
+            // 
+            this.itbis.HeaderText = "itbis";
+            this.itbis.Name = "itbis";
+            // 
+            // receta
+            // 
+            this.receta.HeaderText = "receta";
+            this.receta.Name = "receta";
+            // 
+            // categoria
+            // 
+            this.categoria.HeaderText = "categoria";
+            this.categoria.Name = "categoria";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::eFood.Properties.Resources.iconfinder_67_111124;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(91)))), ((int)(((byte)(140)))));
+            this.button7.FlatAppearance.BorderSize = 2;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(1016, 239);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(34, 35);
+            this.button7.TabIndex = 194;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(852, 247);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(158, 20);
+            this.txtBuscar.TabIndex = 195;
+            this.txtBuscar.Tag = "no vacio";
+            this.txtBuscar.Visible = false;
+            // 
             // platos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1063, 507);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.button7);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label10);
@@ -485,10 +525,10 @@
             this.Load += new System.EventHandler(this.platos_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.efoodDataSet10)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.categoriaBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.efoodDataSet11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.efoodDataSet10)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.recetasBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.efoodDataSet11)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -499,11 +539,6 @@
         private System.Windows.Forms.TextBox txtplato;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn codigo;
-        private System.Windows.Forms.DataGridViewTextBoxColumn descripcion;
-        private System.Windows.Forms.DataGridViewTextBoxColumn porcion;
-        private System.Windows.Forms.DataGridViewTextBoxColumn unidad;
-        private System.Windows.Forms.DataGridViewTextBoxColumn unidad_medida;
         private System.Windows.Forms.ComboBox comboCategoria;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button6;
@@ -535,5 +570,14 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn plato;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itbis;
+        private System.Windows.Forms.DataGridViewTextBoxColumn receta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox txtBuscar;
     }
 }
--- eFood/eFood/platos.cs
+++ eFood/eFood/platos.cs
@@ -12,6 +12,25 @@ namespace eFood
             InitializeComponent();
         }
         int id_plato;
+
+        public void traerPlatos()
+        {
+            DataTable dt = new DataTable();
+            dt.ejecuta("select a.id_plato, a.plato, a.descripcion, a.precio, a.itbis, b.receta, c.DESCRIPCION from platos a inner join recetas b on a.id_receta = b.id_receta inner join categoria c on a.id_categoria = c.ID_CATEGORIA");
+            foreach (DataRow Fila in dt.Rows)
+            {
+                dataGridView1.Rows.Add
+                (
+                    Convert.ToString(Fila["id_plato"]),
+                    Convert.ToString(Fila["plato"]),
+                    Convert.ToString(Fila["descripcion"]),
+                    Convert.ToString(Fila["precio"]),
+                    Convert.ToString(Fila["itbis"]),
+                    Convert.ToString(Fila["receta"]),
+                    Convert.ToString(Fila["DESCRIPCION"])
+                );
+            }
+        }
         private void button5_Click(object sender, EventArgs e)
         {
 
@@ -34,12 +53,13 @@ namespace eFood
         {
             if (MessageBox.Show("Desea Cerrar ", "Aviso", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
             {
-                this.Hide();
+                this.Close();
             }
         }
 
         private void platos_Load(object sender, EventArgs e)
         {
+            traerPlatos();
             // TODO: This line of code loads data into the 'efoodDataSet11.recetas' table. You can move, or remove it, as needed.
             this.recetasTableAdapter.Fill(this.efoodDataSet11.recetas);
             // TODO: This line of code loads data into the 'efoodDataSet10.categoria' table. You can move, or remove it, as needed.
@@ -72,6 +92,25 @@ namespace eFood
             {
 
                 throw;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.ejecuta($"select a.id_plato, a.plato, a.descripcion, a.precio, a.itbis, b.receta, c.DESCRIPCION from platos a inner join recetas b on a.id_receta = b.id_receta inner join categoria c on a.id_categoria = c.ID_CATEGORIA where a.descripcion like ('%" + txtBuscar.Text.Trim() + "%')");
+            foreach (DataRow Fila in dt.Rows)
+            {
+                dataGridView1.Rows.Add
+                (
+                    Convert.ToString(Fila["id_plato"]),
+                    Convert.ToString(Fila["plato"]),
+                    Convert.ToString(Fila["descripcion"]),
+                    Convert.ToString(Fila["precio"]),
+                    Convert.ToString(Fila["itbis"]),
+                    Convert.ToString(Fila["receta"]),
+                    Convert.ToString(Fila["DESCRIPCION"])
+                );
             }
         }
     }
